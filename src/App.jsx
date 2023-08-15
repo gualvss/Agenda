6234c595d8002578b541a5dab557be80488d3038
gualvss <gustavodelima03@gmail.com>
2023-08-15 15:20:55 -0300

    Desenvolvimento Interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <button className="bg-blue-500 text-black">Olá Mundo</button>
+      <body class= "w-screen h-screen bg-black">
+        
+     <div class= "bg-gray-300 p-5 space-y-1">
+     
+     <div class= "text-orange-400 flex justify-start fixed text-xl font-bold">24H Shop</div>
+     
+     <div class= "flex justify-end space-x-4 text-sm relative">
+     <div class= "text-black hover:underline">بلاگ</div>
+     <div class= "text-black hover:underline">تماس با ما</div>
+     <div class= "text-black hover:underline">صفحه اصلی</div>
+     </div>
+     </div>
+     
+<br/>
+<br/>  
+    
+    <div class=" w-full h-20 flex justify-center flex-row items-center space-x-12">
+    
+    <img src="/epic.png" alt="" width={50} height={100}/>
+    <img src="/Steam.png" alt="" width={70} height={100}/>
+    <img src="/Xbox.png" alt="" width={100} height={100}/>
+    <img src="/playstation.jpg" alt="" width={100} height={100}/>
+    </div>
+
+    <br />
+    <br />
+    
+    <div class="flex items-center justify-center">
+      
+      
+      <hr class="w-full border-t  border-white"/>
+      <div class="w-[106px] h-[56px] border rounded-full flex justify-center items-center">
+      <img class="h-10 mx-2" src="/epic.png" title="Imagem centralizada" width={50} height={100}/>
       </div>
+      <hr class="w-full border-t  border-white"/>
+    
+    
+    </div>
+
+
+
+
+
+ 
+ </body>
     </>
   )
 }
