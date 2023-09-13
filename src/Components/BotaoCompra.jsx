da6dd56096922d66b03075b60b4415be492e6722
gualvss <gustavodelima03@gmail.com>
2023-09-13 14:59:38 -0300

    Agenda

--- src/Components/BotaoCompra.jsx
+++ src/Components/BotaoCompra.jsx
@@ -1,7 +1,7 @@
 function BotaoCompra() {
     return (
         
-        <button class="bg-blue-500 text-white font-semibold px-3 py-2 rounded-md hover:underline">Comprar</button>
+        <button class="bg-blue-500 text-white font-semibold px-3 py-2 rounded-md hover:underline"></button>
         
     )
 }
